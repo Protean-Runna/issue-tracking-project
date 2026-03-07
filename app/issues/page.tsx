715cf979e92c8cef5fad0b3af9956e35fbd36cd6
Protean-Runna <209560772+Protean-Runna@users.noreply.github.com>
2026-03-07 18:32:48 +1100

    feat(ui): created basic Issues page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,42 @@
 import Link from "next/link";
-import { Button } from "@radix-ui/themes";
+import { Heading, Button, Text, Card, Flex, Box } from "@radix-ui/themes";
+import prisma from "@/lib/db";
+export default async function issues(){
+    const issues = await prisma.issue.findMany();
 
-export default function issues(){
+
+
     return (
-        <div className="flex min-h-screen ">
-            <Button><Link  href='/issues/new'>New Issue</Link></Button>
+        <div className="min-h-screen ">
+            <Box minHeight="100px">
+                <Heading as="h1" size="9">Issues</Heading> 
+            </Box>
+            <Flex gap="3">
+                <Text size="6"> Current Issues</Text>
+                <Button><Link  href='/issues/new'>New Issue</Link></Button>
+            </Flex>
+            <ul className="flex flex-col gap-3">
+                {issues.map((i) =>(
+                    <li className="" key={i.id}>
+                        <Box maxWidth="450px">
+                            <Card size="2" >
+                                <Flex gap="3" align="center">
+                                    <Box>
+                                        <Text as="div">{i.title}</Text>
+                                        
+                                        <Text as="div">{i.description}</Text>
+                                        <Text as="div" color="red">{i.status}</Text>
+                                    </Box>
+                                </Flex>
+                            </Card>
+                        </Box>
+                        
+                    </li>
+                ))}
+            </ul>
+
         </div>
+
 
     );
 }
