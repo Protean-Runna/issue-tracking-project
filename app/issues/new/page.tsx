7157ca093f138594431503f8512016545a11aec6
Protean-Runna <209560772+Protean-Runna@users.noreply.github.com>
2026-03-07 20:17:09 +1100

    feat: Added form handling

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,33 @@
 'use client'
 import { TextField, TextArea, Button } from "@radix-ui/themes"
+import {useForm} from 'react-hook-form';
+import {issuesAxios} from "@/app/services/apiIssues";
+import { useRouter } from "next/navigation";
+
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 
 
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const {register, handleSubmit} = useForm<IssueForm>()
+
+  
   return (
   
-        <form className='max-w-xl space-y-3' >
-            <TextField.Root variant="soft" radius="large" placeholder="Title"/>
-            <TextArea variant="soft" placeholder="Description" />
-            <Button>Submit Issue</Button>
+        <form 
+        className='max-w-xl space-y-3' onSubmit={handleSubmit(async (data) => {
+          await issuesAxios.create(data);
+          router.push('/issues');
+        })} >
+            <TextField.Root variant="surface" radius="large" placeholder="Title..." {...register('title')}/>
+            <TextArea maxLength={450} variant="surface" placeholder="Description..." {...register('description')} />
+            <Button variant="surface">Submit Issue</Button>
 
         </form>
         
