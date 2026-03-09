6407a1608148972b6d548e036ade6ac46930f446
Protean-Runna <209560772+Protean-Runna@users.noreply.github.com>
2026-03-09 14:35:33 +1100

    feat(ui): updated home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import { Box, Flex, Text, Button, Heading } from "@radix-ui/themes";
 import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
   return (
-    <div className="">
-      Sup 
-    </div>
+    <Box position="relative" top="9">
+      <Flex justify="center" align="center" gap="5" direction="column">
+        <Heading as="h1" size="9">Generic Issue Tracker</Heading>
+        <Text size="7">
+          Track and fix issues <Text weight="bold" color="orange">the easy way</Text>
+        </Text>
+        <Flex align={'center'} gap="3" >
+          <Button size="4" asChild><Link href="/issues">See Issues</Link></Button>
+          <Button size="4" variant="outline" asChild><Link href="/dashboard">Dashboard</Link></Button>
+        </Flex>
+      </Flex>
+    </Box>
   );
 }
