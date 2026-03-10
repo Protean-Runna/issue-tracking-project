769e045dc3c00feabde139ae5f75cdb970f84e43
Protean-Runna <209560772+Protean-Runna@users.noreply.github.com>
2026-03-10 23:16:14 +1100

    added new component to index

--- app/components/index.ts
+++ app/components/index.ts
@@ -3,3 +3,4 @@
 export {default as Link} from "./Link";
 export {default as ErrorMessage} from "./ErrorMessage";
 export {StatusBadge} from "./statusBadge";
+export {default as EditDelBtnGroup} from "./EditDeleteBtnGroup"
