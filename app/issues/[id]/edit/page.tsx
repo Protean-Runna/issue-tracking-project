fe0ea3e56cdd71b40a5c6aae718fc65a17566e19
Protean-Runna <209560772+Protean-Runna@users.noreply.github.com>
2026-03-10 23:35:24 +1100

    Feat: Separated and Reimplemented Issue Form  Component for New Issue and Edit Issue Pages

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,10 +1,30 @@
+import { notFound } from "next/navigation";
+import IssueForm from "../../_components/IssueForm";
+import prisma from "@/lib/db";
+import delay from "delay";
+interface Props {
+    params: {id:string}
+}
 
+const EditPage =  async ({params}: Props) => {
+    const {id} = await params;  // await has no effect my foot
 
-export default function EditPage(){
+  const issueId = parseInt(id);
+  const issue = await prisma.issue.findUnique({       
+        where : {id: issueId},
+    });
+    if (!issue) {
+        return notFound();
+      }
+      await delay(1000);
     return (
-        <div className="flex min-h-screen justify-center ">
+        <div>
             <p className="">Hello there! This is the Edit page</p>
+            <IssueForm issue={issue}/>
         </div>
 
     );
 }
+
+
+export default EditPage;
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,65 +1,11 @@
-'use client'
+
 import React from "react";
-import { useState } from "react";
-import dynamic from "next/dynamic";
-import "easymde/dist/easymde.min.css";
-import { TextField, Callout, Button, Spinner } from "@radix-ui/themes";
-import {useForm, Controller} from 'react-hook-form';
-import {issuesAxios} from "@/app/services/apiIssues";
-import { useRouter } from "next/navigation";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { IssueSchema } from "@/lib/ValidationSchemas";
-import {z} from 'zod';
-import ErrorMessage from "@/app/components/ErrorMessage";
-
-const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
-  ssr: false,
-  loading: () => <p>Loading editor...</p>,
-});
-
-type IssueForm = z.infer<typeof IssueSchema>;
+import IssueForm from "../_components/IssueForm";
 
 const NewIssuePage = () => {
-  const router = useRouter();
-  const {register, control, handleSubmit, formState: {errors}} = useForm<IssueForm>({
-    resolver: zodResolver(IssueSchema)
-  })
-  const [error, setError] = useState('');
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const onSubmit = handleSubmit(async (data) => {  
-          try {
-            setIsSubmitting(true);
-            await issuesAxios.create(data);
-            router.push('/issues');            
-          } catch (error) {
-            setIsSubmitting(false);
-            console.log(error);
-            setError('An unexpected error has occurred. Please try again.');
-          }          
-        })
 
-  
   return (
-        <div className="max-w-xl">
-          {error && 
-          <Callout.Root color="red">
-            <Callout.Text>{error}</Callout.Text>
-            </Callout.Root>
-            }
-        
-  
-          <form 
-          className=' space-y-3 space-x-5 mt-2' onSubmit={onSubmit} >
-              <ErrorMessage>{errors.title?.message}</ErrorMessage>
-              <TextField.Root variant="surface" radius="large" placeholder="Title..." {...register('title')}/>
-              
-              <ErrorMessage>{errors.description?.message}</ErrorMessage>
-              <Controller name="description" control={control} render={({field}) => <SimpleMDE  placeholder="Description..." {...field} />} />
-              <Button disabled={isSubmitting} variant="surface" size={"3"}>Submit Issue { isSubmitting && <Spinner/> || null}</Button>
-          </form>
-        </div>
-        
-
+    <IssueForm/>
   )
 }
 
