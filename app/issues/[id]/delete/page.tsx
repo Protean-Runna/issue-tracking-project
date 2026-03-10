a5ce44f710672115eef4447c151b011aaf901c7d
Protean-Runna <209560772+Protean-Runna@users.noreply.github.com>
2026-03-10 22:03:47 +1100

    Initial Commit: Edit and Delete Pages & loading state

---
+++ app/issues/[id]/delete/page.tsx
@@ -0,0 +1,8 @@
+export default function DeletePage(){
+    return (
+        <div className="flex min-h-screen justify-center ">
+            <p className="">Hello there! This is the Delete page</p>
+        </div>
+
+    );
+}
---
+++ app/issues/[id]/edit/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function EditPage(){
+    return (
+        <div className="flex min-h-screen justify-center ">
+            <p className="">Hello there! This is the Edit page</p>
+        </div>
+
+    );
+}
