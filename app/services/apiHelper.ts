709da9768753d0c8c76bd92c71a2973705447248
Protean-Runna <209560772+Protean-Runna@users.noreply.github.com>
2026-03-11 00:04:18 +1100

    chore: changed update from put to patch.
    This was a holdover from this being used on one of my older projects

--- app/services/apiHelper.ts
+++ app/services/apiHelper.ts
@@ -3,14 +3,14 @@ import apiClient from "./apiClient";
 export const apiService = {
   getAll: <T = any>(resource: string) => apiClient.get<T>(`/api/${resource}`),
   getById: <T = any>(resource: string, id: string) => apiClient.get<T>(`/api/${resource}/${id}`),
-  // JSON POSTS AND PUTS
+  // JSON POSTS AND PATCHES
   create: <T = any, U = any>(resource: string, data: T) => apiClient.post<U>(`/api/${resource}/create`, data),
-  update: <T = any, U = any>(resource: string, id: string, data: T) => apiClient.put<U>(`/api/${resource}/update/${id}`, data),
+  update: <T = any, U = any>(resource: string, id: string, data: T) => apiClient.patch<U>(`/api/${resource}/update/${id}`, data),
 
-  // MULTI POSTS AND PUTS
+  // MULTI POSTS AND PATCHES
   createMultipart: <T = any, U = any>(resource: string, formData: FormData) =>
     apiClient.post<U>(`/api/${resource}/create`, formData, { headers: { "Content-Type": "multipart/form-data" } }),
   updateMultipart: <T = any, U = any>(resource: string, id: string, formData: FormData) =>
-    apiClient.put<U>(`/api/${resource}/update/${id}`, formData, { headers: { "Content-Type": "multipart/form-data" } }),
+    apiClient.patch<U>(`/api/${resource}/update/${id}`, formData, { headers: { "Content-Type": "multipart/form-data" } }),
   remove: <T = any>(resource: string, id: string) => apiClient.delete<T>(`/api/${resource}/delete/${id}`),
 };
