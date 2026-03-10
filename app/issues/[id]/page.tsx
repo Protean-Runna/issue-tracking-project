e2a15ce2da71b41f036fe116d0626f9f1e6bddb5
Protean-Runna <209560772+Protean-Runna@users.noreply.github.com>
2026-03-10 16:39:07 +1100

    refactor(ui): Updated issue card styling

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,14 +1,14 @@
 
 import { notFound } from 'next/navigation';
 import { issuesAxios } from '@/app/services/apiIssues';
-import { Text, Card, Flex, Box } from '@radix-ui/themes';
+import { Text, Card, Flex, Box, DataList } from '@radix-ui/themes';
 import { StatusBadge } from '@/app/components/statusBadge';
 async function fetchIssue(id:string) {
     const res = await issuesAxios.getSingle(id);
     if (!res){
             return null;
         }
-        const issue = await res.data;
+        const issue = res.data;
         return issue;
 }
 
@@ -21,18 +21,43 @@ const issueDetailsPage = async ({params}: {params: Promise<{id:string}>}) => {
     if (!issue){
        return notFound();
     }
+
     return(
-        <Box maxWidth={"450px"}>
-            <Card>
-                <Flex justify={"center"} align={"center"} direction={"column"} gap={"2"}>
-                    <Text>Issue: {issue?.title}</Text>
-                    <Box minHeight={"50px"}>
-                        <Text>{issue?.description}</Text>
-                    </Box>
-                    <StatusBadge dbStatus={issue.status || "grey"} />
-                </Flex>
-            </Card>
-        </Box>
+        <Flex justify={"center"} align={"center"}>
+            <Box maxWidth={"500px"}>
+                <Card size={"3"}>
+                    <DataList.Root mb={"5"} size={"2"}  >
+                         {/** Title*/}
+                        <DataList.Item align={"center"}>
+                           
+                        <DataList.Label>Issue:</DataList.Label>
+                        <DataList.Value>{issue?.title}</DataList.Value>
+                        </DataList.Item>
+                        {/** Status */}
+                        <DataList.Item>
+                        <DataList.Label>Status:</DataList.Label>
+                        <DataList.Value><StatusBadge dbStatus={issue.status || "grey"} /></DataList.Value>
+                        </DataList.Item>
+                        {/** Created */}
+                        <DataList.Item>
+                        <DataList.Label>Created:</DataList.Label>
+                        <DataList.Value>{issue?.createdAt}</DataList.Value>
+                        </DataList.Item>
+                        {/** Updated */}
+                        <DataList.Item>
+                        <DataList.Label>Updated:</DataList.Label>
+                        <DataList.Value>{issue?.updatedAt}</DataList.Value>
+                        </DataList.Item>
+
+                    </DataList.Root>
+                        <Box minHeight={"50px"}>
+                            <Text>{issue?.description}</Text>
+                        </Box>
+                        
+                   
+                </Card>
+            </Box>
+        </Flex>
        
     );
 }
