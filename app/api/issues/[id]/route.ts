07c209a446952954ff21a956a639df79fb01e0e4
Protean-Runna <209560772+Protean-Runna@users.noreply.github.com>
2026-03-11 14:01:28 +1100

    feat: built API route for deleting issues.

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,72 +1,123 @@
 // Route for getting individual Issues
-import {NextRequest,NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { IssueSchema } from "@/lib/ValidationSchemas";
 import prisma from "@/lib/db";
+import { Prisma } from "@/app/generated/prisma/client";
 
-
-export async function GET(request: NextRequest, {params}: {params: Promise<{id: string}>}) {
-    // TO DO: Fix this so that it can work with the IssueData Component
-    try {
-        const {id} = await params;
-        const issueId = parseInt(id);
-        if (isNaN(issueId)){
-            return NextResponse.json({message:"Invalid ID"},{status: 400})
-        };
-    const issue = await prisma.issue.findUnique({       
-        where : {id: issueId},
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  // UPDATE: It now works with the IssueData Component, huzzah!
+  try {
+    const { id } = await params;
+    const issueId = parseInt(id);
+    if (isNaN(issueId)) {
+      return NextResponse.json({ message: "Invalid ID" }, { status: 400 });
+    }
+    const issue = await prisma.issue.findUnique({
+      where: { id: issueId },
     });
 
+    if (!issue) {
+      return NextResponse.json(
+        { message: "Issue could not be found" },
+        { status: 404 },
+      );
+    }
 
-    if (!issue){
-        return NextResponse.json({message: "Issue could not be found"}, {status:404})
-    };
-    
     const uiIssue = {
-        ...issue,
-        status: issue.status.toUpperCase(),
-        createdAt: issue.createdAt,
-        updatedAt: issue.updatedAt,
-    }
-    
-    return NextResponse.json(uiIssue, {status:200});
-    } catch (error) {
-        console.error('Error fetching issue:', error);
-        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
-    }
- 
-};
+      ...issue,
+      status: issue.status.toUpperCase(),
+      createdAt: issue.createdAt,
+      updatedAt: issue.updatedAt,
+    };
 
-export async function PATCH(request: NextRequest, {params}: {params: {id: string}}){
-    const body = await request.json();
-    const validation = IssueSchema.safeParse(body);
-    
-    if (!validation.success){
-        return NextResponse.json(validation.error.issues, {status:400})
-    }
-    try {
-        const {id} = await params;
-        const issueId = parseInt(id);
-
-        const issue = prisma.issue.findUnique({
-            where: {id: issueId}
-        })
-        if (!issue){
-        return NextResponse.json({message: "Issue could not be found"}, {status:404})
-        };
-
-        const updatedIssue = await prisma.issue.update({
-            where: { id: issueId},
-            data: {
-                title: body.title,
-                description: body.description,
-                updatedAt: new Date()
-            }
-            });
-            return NextResponse.json(updatedIssue);
-    } catch (error) {
-        console.error('Error updating issue:', error);
-        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
-        
-    }
+    return NextResponse.json(uiIssue, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching issue:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
 }
 
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const body = await request.json();
+  const validation = IssueSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.issues, { status: 400 });
+  }
+  try {
+    const { id } = await params;
+    const issueId = parseInt(id);
+    if (isNaN(issueId)) {
+      return NextResponse.json({ message: "Invalid ID" }, { status: 400 });
+    }
+
+    const issue = prisma.issue.findUnique({
+      where: { id: issueId },
+    });
+    if (!issue) {
+      return NextResponse.json(
+        { message: "Issue could not be found" },
+        { status: 404 },
+      );
+    }
+
+    const updatedIssue = await prisma.issue.update({
+      where: { id: issueId },
+      data: {
+        title: body.title,
+        description: body.description,
+        updatedAt: new Date(),
+      },
+    });
+    return NextResponse.json(updatedIssue);
+  } catch (error) {
+    console.error("Error updating issue:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  try {
+    const { id } = await params;
+    const issueId = parseInt(id);
+    if (isNaN(issueId)) {
+      return NextResponse.json({ message: "Invalid ID" }, { status: 400 });
+    }
+
+    await prisma.issue.delete({
+      where: { id: issueId },
+    });
+
+    return NextResponse.json(
+      { message: `Issue ${issueId} has been deleted` },
+      { status: 204 },
+    );
+  } catch (error) {
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2025"
+    ) {
+      return NextResponse.json({ message: "Issue not found" }, { status: 404 });
+    }
+    console.error("Error deleting issue:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
