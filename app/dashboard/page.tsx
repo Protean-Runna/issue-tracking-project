26f614348fb231220257ee6d79b8b4f7dbb2d60d
Protean-Runna <209560772+Protean-Runna@users.noreply.github.com>
2026-03-14 15:56:19 +1100

    feat: implemented basic dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,31 @@
-export default function dashboard(){
-    return (
-        <div className="flex min-h-screen">
-            <p className="">Hello there! This is the dashboard page</p>
-        </div>
+import { Box, Card, Heading, Flex, Grid, Text } from "@radix-ui/themes";
+import prisma from "@/lib/db";
 
-    );
+export default async function dashboard() {
+  const issueCounts = {
+    open: await prisma.issue.count({ where: { status: "OPEN" } }),
+    inProgress: await prisma.issue.count({ where: { status: "IN_PROGRESS" } }),
+    closed: await prisma.issue.count({ where: { status: "IN_PROGRESS" } }),
+  };
+  // TO DO: In the future, we'll need some more stuff, like a chart and a way to show recent issues
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Heading as="h1" size="9">Dashboard</Heading>
+      <Heading as="h2" size={"7"}> Current Issues</Heading>
+      <Grid gap={"3"} columns={"3"}>
+        <Card>
+          <Heading as="h3" size={"5"}>Open</Heading>
+          <Text size={"2"}>{issueCounts.open}</Text>
+        </Card>
+        <Card>
+          <Heading as="h3" size={"5"}>In Progress</Heading>
+          <Text size={"2"}>{issueCounts.inProgress}</Text>
+        </Card>
+        <Card>
+          <Heading as="h3" size={"5"}>Closed</Heading>
+          <Text size={"2"}>{issueCounts.closed}</Text>
+        </Card>
+      </Grid>
+    </div>
+  );
 }
