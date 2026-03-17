55a4f4579d927dabacfc4a5d851b1dd9710d2379
Protean-Runna <209560772+Protean-Runna@users.noreply.github.com>
2026-03-17 20:26:52 +1100

    refactor: changed fetching from API to fetching from the server itself

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,24 +1,27 @@
 import { notFound } from "next/navigation";
-import { ISSUES_AXIOS } from "@/app/services/apiResourceFactory";
 import { Flex, Container } from "@radix-ui/themes";
 import delay from "delay";
 import IssueData from "./IssueData";
-import axios from "axios";
+import prisma from "@/lib/db";
 interface Props {
   params: { id: string };
 }
 
 const issueDetailsPage = async ({ params }: Props) => {
-  // Commented out do to some funky interactions with the api
+  // So I don't really need to do api calls when it's running on it's own server.
   //
+  
   const { id } = await params;
-  await delay(1000);
-  try {
-    const res = await ISSUES_AXIOS.getSingle(id);
+  
+  const issueId = parseInt(id);
+  if (isNaN(issueId)) notFound();
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: issueId },
+  });
+  if (!issue) notFound();
     await delay(1000);
-
-    const issue = res.data;
-
+    
     return (
       <Flex justify={"center"} align={"center"} className="w-full">
         <Container size={"3"} className="min-w-0 w-full">
@@ -26,12 +29,8 @@ const issueDetailsPage = async ({ params }: Props) => {
         </Container>
       </Flex>
     );
-  } catch (error: unknown) {
-    if (axios.isAxiosError(error) && error.response?.status === 404) {
-      return notFound();
-    }
-    throw error;
-  }
+
+    
 };
 
 export default issueDetailsPage;
