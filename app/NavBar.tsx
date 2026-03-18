cc26a51bd8473e4bbc82eeb7dfb9742bdaada0d7
Protean-Runna <209560772+Protean-Runna@users.noreply.github.com>
2026-03-18 23:25:56 +1100

    refactor: segmented Nav links and AuthStatus from navbar component & updated styles

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,64 +1,100 @@
 "use client";
 
-import { Box, Container, Flex } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { IoIosBulb } from "react-icons/io";
 
 const NavBar = () => {
+  return (
+    <nav
+      style={{ backgroundColor: "var(--black-a10)" }}
+      className="mb-5 py-3 px-5 border-b h-14"
+    >
+      {" "}
+      <Container size={"4"}>
+        <Flex justify={"between"}>
+          <Flex align={"center"} gap={"3"}>
+            <Link href={"/"}>
+              <IoIosBulb size={"20px"} />
+            </Link>
+            <NavLinks />
+          </Flex>
+          <AuthStatus />
+        </Flex>
+      </Container>
+    </nav>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  if (status === "loading") return null;
+  if (status === "unauthenticated")
+    return (
+      <Link className="nav-link" href="/api/auth/signin">
+        Login
+      </Link>
+    );
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            className="cursor-pointer"
+            radius="full"
+            size={"2"}
+            src={session!.user!.image!}
+            fallback="?"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size={"3"}>{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href="/api/auth/signout">Log out</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
+
+const NavLinks = () => {
   const links = [
     { label: "About", href: "/about" },
     { label: "Issues", href: "/issues" },
     { label: "Dashboard", href: "/dashboard" },
   ];
   const currentPath = usePathname();
-  const { status, data: session } = useSession();
   console.log(currentPath);
 
   return (
-    <nav
-      style={{ backgroundColor: "var(--black-a10)" }}
-      className="mb-5 py-4 px-5 border-b h-14"
-    >
-      {" "}
-      <Container>
-        <Flex justify={"between"}>
-          <Flex align={"center"} gap={"3"}>
-            <Link href={"/"}>
-              <IoIosBulb size={"20px"} />
-            </Link>
-            <ul className="flex space-x-6">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    className={`
-                                hover:text-amber-500  
-                                transition-colors
-                                ${
-                                  link.href === currentPath
-                                    ? "text-amber-300"
-                                    : "text-zinc-50"
-                                }`}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </Flex>
-          <Box>
-            {status === "authenticated" && (
-              <Link href="/api/auth/signout">Log out</Link>
-            )}
-            {status === "unauthenticated" && (
-              <Link href="/api/auth/signin">Login</Link>
-            )}
-          </Box>
-        </Flex>
-      </Container>
-    </nav>
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={`
+               nav-link
+                ${link.href === currentPath && "text-amber-300!"}`}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 };
 
