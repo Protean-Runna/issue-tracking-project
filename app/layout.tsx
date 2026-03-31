76ad2403018f7745e4796e4106d7b255b2cc3c21
Protean-Runna <209560772+Protean-Runna@users.noreply.github.com>
2026-03-31 16:17:03 +1100

    chore: minor changes and route fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Issue Tracker",
+  title: "Issue Tracky",
   description: "A little Issue Tracker project",
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
 import { Box, Flex, Text, Button, Heading } from "@radix-ui/themes";
-import Image from "next/image";
 import Link from "next/link";
 export default function Home() {
   return (
     <Box position="relative" top="9">
       <Flex justify="center" align="center" gap="5" direction="column">
-        <Heading as="h1" size="9">Generic Issue Tracker</Heading>
+        <Heading as="h1" size="9">Issue Tracky</Heading>
         <Text size="7">
           Track and fix issues <Text weight="bold" style={{color:'var(--accent-10)'}}>the easy way</Text>
         </Text>
         <Flex align={'center'} gap="3" >
-          <Button size="4" asChild><Link href="/issues">See Issues</Link></Button>
+          <Button size="4" asChild><Link href="/issues/list">See Issues</Link></Button>
           <Button size="4" variant="outline" asChild><Link href="/dashboard">Dashboard</Link></Button>
         </Flex>
       </Flex>
