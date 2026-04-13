dbc2bab673d34ab9499ff56278e309bbba0c22e5
Protean-Runna <209560772+Protean-Runna@users.noreply.github.com>
2026-04-13 13:26:38 +1000

    fix: params are now wrapped ith a promise

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -5,9 +5,48 @@ import prisma from "@/lib/db";
 import { Prisma } from "@/app/generated/prisma/client";
 import { auth } from "@/auth";
 
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  // UPDATE: It now works with the IssueData Component, huzzah!
+  try {
+    const { id } = await params;
+    const issueId = parseInt(id);
+    if (isNaN(issueId)) {
+      return NextResponse.json({ message: "Invalid ID" }, { status: 400 });
+    }
+    const issue = await prisma.issue.findUnique({
+      where: { id: issueId },
+    });
+
+    if (!issue) {
+      return NextResponse.json(
+        { message: "Issue could not be found" },
+        { status: 404 },
+      );
+    }
+
+    const uiIssue = {
+      ...issue,
+      status: issue.status.toUpperCase(),
+      createdAt: issue.createdAt,
+      updatedAt: issue.updatedAt,
+    };
+
+    return NextResponse.json(uiIssue, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching issue:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
+
 export async function PATCH(
   request: NextRequest,
-  { params }: { params: { id: string } },
+  { params }: { params: Promise<{ id: string }> },
 ) {
   const session = await auth();
   if (!session){
@@ -63,7 +102,7 @@ export async function PATCH(
 
 export async function DELETE(
   request: NextRequest,
-  { params }: { params: { id: string } },
+  { params }: { params: Promise<{ id: string }> },
 ) {
   const session = await auth();
     if (!session){
