a00982e33227762a0dce367318bf001fd4fc2a4e
Protean-Runna <209560772+Protean-Runna@users.noreply.github.com>
2026-04-13 13:20:03 +1000

    fix: removed GET route in issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -5,45 +5,6 @@ import prisma from "@/lib/db";
 import { Prisma } from "@/app/generated/prisma/client";
 import { auth } from "@/auth";
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { id: string } },
-) {
-  // UPDATE: It now works with the IssueData Component, huzzah!
-  try {
-    const { id } = await params;
-    const issueId = parseInt(id);
-    if (isNaN(issueId)) {
-      return NextResponse.json({ message: "Invalid ID" }, { status: 400 });
-    }
-    const issue = await prisma.issue.findUnique({
-      where: { id: issueId },
-    });
-
-    if (!issue) {
-      return NextResponse.json(
-        { message: "Issue could not be found" },
-        { status: 404 },
-      );
-    }
-
-    const uiIssue = {
-      ...issue,
-      status: issue.status.toUpperCase(),
-      createdAt: issue.createdAt,
-      updatedAt: issue.updatedAt,
-    };
-
-    return NextResponse.json(uiIssue, { status: 200 });
-  } catch (error) {
-    console.error("Error fetching issue:", error);
-    return NextResponse.json(
-      { message: "Internal Server Error" },
-      { status: 500 },
-    );
-  }
-}
-
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } },
